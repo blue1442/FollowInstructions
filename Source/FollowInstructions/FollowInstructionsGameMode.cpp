562d609149df539d5b36f021ac761dc3ef14b8ac
blue1442 <36316882+blue1442@users.noreply.github.com>
2020-03-04 17:00:33 -0600

    got hud working

--- Source/FollowInstructions/FollowInstructionsGameMode.cpp
+++ Source/FollowInstructions/FollowInstructionsGameMode.cpp
@@ -8,9 +8,18 @@ AFollowInstructionsGameMode::AFollowInstructionsGameMode()
 {
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/FollowInstructions/Blueprints/MainCharBps/MainChar_BP"));
+	static ConstructorHelpers::FClassFinder<UObject> PlayerHUDClass(TEXT("/Game/FollowInstructions/Blueprints/MainCharBps/Hud"));
+
 	if (PlayerPawnBPClass.Class != NULL) {
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	} else {
 		UE_LOG(LogTemp, Warning, TEXT("TCould not find default player pawn class, was the file moved by gnomes?"));
+	}
+
+	if (PlayerHUDClass.Class != NULL) {
+		HUDClass = PlayerHUDClass.Class;
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("TCould not find default player hud class, was the file moved by gnomes?"));
 	}
 }
